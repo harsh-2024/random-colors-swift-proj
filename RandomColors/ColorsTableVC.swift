e67f4cd742372cb2545ae6e6de8e9cc4972b1add
HarshTripathi <tripathiharsh16@gmail.com>
2024-04-11 23:33:58 +0530

    implemented colors BL in TableView

--- RandomColors/ColorsTableVC.swift
+++ RandomColors/ColorsTableVC.swift
@@ -8,18 +8,34 @@
 import UIKit
 
 class ColorsTableVC: UIViewController{
+    var colorsArray : [UIColor] = [];
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        addRandomColors()
+    }
+    
+    func addRandomColors(){
+        colorsArray.append(createRandomColors());
+    }
+    
+    func createRandomColors() -> UIColor {
+        let randomColor = UIColor(red: CGFloat.random(in:0...1), green: CGFloat.random(in:0...1), blue: CGFloat.random(in:0...1), alpha: CGFloat.random(in: 0...1))
+        return randomColor
     }
 }
 extension ColorsTableVC: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 100
+        return colorsArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ColorCells")
+        else{
+            return UITableViewCell()
+        }
+        cell.backgroundColor = colorsArray[indexPath.row];
+        return cell;
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: "ToColorsDetailVC", sender: nil)
